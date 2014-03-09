8f3c44c3e8df442a905462af644010407666a66d
zakaria.arrama@gmail.com <zakaria.arrama@gmail.com@09204b4f-c2eb-d429-f2b5-1f56cd63b9aa>
2014-03-09 04:09:21 +0000

    add linkdescription entity

--- FDL-Optimized/src/com/fdl/dao/FdlDao.java
+++ FDL-Optimized/src/com/fdl/dao/FdlDao.java
@@ -11,34 +11,11 @@ import com.fdl.entity.Node;
 
 public class FdlDao extends HibernateDaoSupport{
 
-	public Graph getGraph(){
-		Graph graph = new Graph();
-
-		Node n1 = new Node("N1",0);
-		Node n2 = new Node("N2",1);
-		Node n3 = new Node("N3",2);
-
-		Link link1 = new Link(0, 1, 1);
-		Link link2 = new Link(0, 2, 2);
-		Link link3 = new Link(1, 2, 3);
-
-		graph.getLinks().add(link1);
-		graph.getLinks().add(link2);
-		graph.getLinks().add(link3);
-		
-		graph.getNodes().add(n1);
-		graph.getNodes().add(n2);
-		graph.getNodes().add(n3);
-		
-		return graph;	
-	}
-
-
 	public Graph getGraph2(){
 		//getHibernateTemplate().save(link);	
 
 		Set<Node> nodes = new HashSet<Node>(getHibernateTemplate().find("From Node"));
-		Set<Link> links =new HashSet<Link>(getHibernateTemplate().find("From Link"));
+		Set<Link> links = new HashSet<Link>(getHibernateTemplate().find("From Link"));
 		Graph graph = new Graph(nodes,links);
 		return graph;
 	}
