4ecc66adecf3eafcdaee9b2c9dd3a377f9ed74da
zakaria.arrama@gmail.com <zakaria.arrama@gmail.com@09204b4f-c2eb-d429-f2b5-1f56cd63b9aa>
2014-03-16 01:26:52 +0000

    Add servlet "GetGraph" to replace  getGraph.jsp

---
+++ FDL-Optimized/src/com/fdl/servlets/GetGraph.java
@@ -0,0 +1,36 @@
+package com.fdl.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import com.fdl.dao.FdlDao;
+import com.fdl.entity.Graph;
+import com.google.gson.Gson;
+
+@Component("getGraph")
+public class GetGraph implements HttpRequestHandler {
+
+	@Autowired
+	private FdlDao fdlDao;
+	
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("application/json;charset=UTF-8");
+		Gson gson = new Gson();
+		Graph graph = fdlDao.getGraph2();
+		PrintWriter out = response.getWriter();
+	    out.println(gson.toJson(graph));
+	}
+
+
+}
