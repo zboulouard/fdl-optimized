425004f7057867f1c1ba3077c2a637580fcc0c32
zakaria.arrama@gmail.com <zakaria.arrama@gmail.com@09204b4f-c2eb-d429-f2b5-1f56cd63b9aa>
2014-03-16 01:06:50 +0000

    Use servlet to return json

--- FDL-Optimized/src/com/fdl/dao/FdlDao.java
+++ FDL-Optimized/src/com/fdl/dao/FdlDao.java
@@ -13,11 +13,16 @@ public class FdlDao extends HibernateDaoSupport{
 
 	public Graph getGraph2(){
 		//getHibernateTemplate().save(link);	
-
 		Set<Node> nodes = new HashSet<Node>(getHibernateTemplate().find("From Node"));
 		Set<Link> links = new HashSet<Link>(getHibernateTemplate().find("From Link"));
 		Graph graph = new Graph(nodes,links);
 		return graph;
+	}
+	
+	public void addGraph(){
+		Graph g = new Graph();
+		g.setName("g1");
+		getHibernateTemplate().save(g);
 	}
 
 }
